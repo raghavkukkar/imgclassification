c14b6683e0d7ff47adf1b3149767a175f08779be
raghav kukkar <raghavkukkar9@gmail.com>
2021-03-08 17:34:07 +0530

    added softmax and relu

--- nn.py
+++ nn.py
@@ -1,34 +1,55 @@
+#ULTIMATE TODO PRACTISE INTEGRAL TO ROMAN NUMERAL AND ONE MORE MEDIUM LEETCODE PROBLEM
+
+
 import numpy as np
 from impscript import getData
 
-# DESIGN CONSIDERATION COULD HAVE IMPLEMENTED IT DIFFERENTLY
+# DESIGN CONSIDERATION COULD HAVE IMPLEMENTED IT DIFFERENTLY AND YES IT LOOKS BAD BUT I WANTED TO STATIC METHODS AND CLASSES, BECAUSE IT JUST 
+# MAKES THE STRUCTURE OF CODE CLEAN FOR ME SO THIS IS HOW IT IS IMPLEMENTED BYE! 
+
 class Activations:
     @staticmethod
     def sigmoid(x):
         return 1.0/(1.0 + np.exp(-x))
+    @staticmethod
+    def relu(x):
+       
+        return np.maximum(0,x)
+    @staticmethod
+    def softmax(x):
+        y = np.exp(x)
+        return y/np.sum(y,axis = 0)
+        
+#TODO SOFTMAX ACTIVATION BACKGATE - I'M NOTGOING TO IMPLEMENT TODAY BECAUSE I COULD NOT REALLY UNDERSTAND THE DERIVATIVE.
 class Backgates:
     @staticmethod
     def sigPrime(x,y =1):
         return y*Activations.sigmoid(x)*(1 - Activations.sigmoid(x))
-
+    @staticmethod 
+    def reluPrime(x,y = 1):
+        x[x<= 0 ] = 0
+        return y*(x)
+    
 # MAIN CLASS FOR NETWORK 
 
-# WORK TO DO - GENERALIZE THE BACKPROP ALGO
+# WORK TO DO - GENERALIZE THE BACKPROP ALGO AND ADD CROSS ENTROPY LOSS CALCULATION AND TRY IT WITH SOFTMAX RELU SIGMOID
+#TODO CONTINUE - ADD MINI BATCH RANDOMIZATION AND TRY IF WE GET BETTER RESULTS
 class Network:
-    def __init__(self,size,_input,_tags , _validation ,_validtags,reg ):
+    def __init__(self,size,_input,_tags , _validation ,_validtags,reg ,methods ):
         self.input = _input/255
+        self.reg = reg
         self.valid = _validation/255
         self.vtags = _validtags
         self.tags = _tags
         self.num_layers = len(size) - 1
         self.sizes = size
-        self.bias = [0.01*np.random.randn(y,1) for y in (self.sizes[1:])]
-
-        self.weights = [0.01*np.random.randn(y,x) for x , y in zip(self.sizes[:-1],self.sizes[1:])]
+        self.bias = [0.001*np.random.randn(y,1) for y in (self.sizes[1:])]
+        self.methods = methods
+        self.weights = [0.001*np.random.randn(y,x) for x , y in zip(self.sizes[:-1],self.sizes[1:])]
         
         self.gdw = []
         self.gdb = []
-        self.convTags()
+        
     
     def convTags(self):
         x = np.zeros((self.sizes[-1] , self.tags.shape[0]))
@@ -37,24 +58,23 @@ class Network:
         
     def forwardPass(self):
         a = self.valid
-        
-        for b,w in zip(self.bias , self.weights):
-            a = Activations.sigmoid(np.matmul(w,a) + b)
+        for b,w,m in zip(self.bias , self.weights,self.methods):
+            a = m(np.matmul(w,a) + b)
         return a
 
-# SOME BAD SHIT IS HAPPENING HERE DEBUGGING ...
+
     def update(self,rate):
         for i in range(self.num_layers):
-            self.bias[i] = self.bias[i] - (rate*self.gdb.pop())
-            
-            self.weights[i] = self.weights[i] - rate*self.gdw.pop()
+            self.bias[i] += -(rate*self.gdb.pop())
+            self.weights[i] += -(rate*self.reg*self.gdw.pop())
     
      
     def accuracy(self): 
         y=  self.forwardPass().argmax(axis = 0 )
-        return np.mean(self.vtags == y)
         
-    def train(self , rate,epochs , groups):
+        return np.mean(y == self.vtags)
+        
+    def train(self , rate,epochs , groups ):
       
         
         for e in range(epochs):
@@ -64,18 +84,18 @@ class Network:
                     activate = [self.input[:,t: t + groups]]
                     zs = []
                     
-                    #forward pass
-                    for x , y in zip(self.weights,self.bias):
+                    #FORWARD PASS OR FORWARD PASS
+                    for x , y , m in zip(self.weights,self.bias,self.methods):
                         zs.append(np.matmul(x,activate[-1]) + y)
-                        activate.append(Activations.sigmoid(zs[-1]))
+                        activate.append(m(zs[-1]))
         
-                    #backward pass
-                    x = Backgates.sigPrime(zs[-1],activate[-1] - self.tags[:,t: t + groups] )
+                    #BACKWARD PASS OR BACKPROP
+                    x = activate[-1]
+                    x[self.tags[t:t+groups],np.arange(0,groups)] -=1 #might be wrong .... most probably wrong NEED CHANGES AND RESEARCH
                     self.gdb.append(x.sum(axis = 1,keepdims = True))
-                    
                     self.gdw.append(np.matmul(x,activate[-2].T))
                     x = np.matmul(self.weights[-1].T , x)
-                    x = Backgates.sigPrime(zs[-2],x)
+                    x = Backgates.reluPrime(zs[-2],x)
                     self.gdb.append(x.sum(axis = 1 , keepdims = True))
                     self.gdw.append(np.matmul(x,activate[-3].T))
                     self.update(rate)
@@ -85,9 +105,9 @@ class Network:
 data , labels = getData('./cifar-10-batches-py/train')
 data= data.T
 
-nn = Network(((32*32*3),100,10), data[:,0:40000], labels[0:40000], data[:,40000:50000], labels[40000:50000])
+nn = Network(((32*32*3),50,10), data[:,0:40000], labels[0:40000], data[:,40000:50000], labels[40000:50000],0.5,(Activations.relu,Activations.softmax))
 
-nn.train(0.01,90,50)
+nn.train(0.0001,2000,100)
 
 acc= nn.accuracy()
 
